ab16efcd5b2ab6a94932d9094ec60bef2c3c7c2a
Vitaliy Nikolaev <faktor600gmail.com>
2020-09-28 13:34:43 +0400

    исправления GetJson

--- app/src/main/java/com/simbir/kotlinpractice/data/json/GetJsonCase.kt
+++ app/src/main/java/com/simbir/kotlinpractice/data/json/GetJsonCase.kt
@@ -1,8 +1,9 @@
 package com.simbir.kotlinpractice.data.json
 
 import io.reactivex.rxjava3.core.Single
+import java.lang.reflect.Type
 
 interface GetJsonCase {
 
-    fun getJsonList(requiredClass: Class<Any>): Single<Any>
+    fun getJsonList(type: Type): Single<Any>
 }
--- app/src/main/java/com/simbir/kotlinpractice/data/json/GetJsonCaseImpl.kt
+++ app/src/main/java/com/simbir/kotlinpractice/data/json/GetJsonCaseImpl.kt
@@ -3,11 +3,12 @@ package com.simbir.kotlinpractice.data.json
 import com.google.gson.Gson
 import io.reactivex.rxjava3.core.Single
 import java.io.InputStream
+import java.lang.reflect.Type
 
 class GetJsonCaseImpl(private val inputStream: InputStream?, private val gson: Gson): GetJsonCase {
 
-    override fun getJsonList(requiredClass: Class<Any>): Single<Any> {
-        return Single.just(gson.fromJson(initJson()?: "", requiredClass))
+    override fun getJsonList(type: Type): Single<Any> {
+        return Single.just(gson.fromJson(initJson().orEmpty(), type))
     }
 
     private fun initJson(): String?{
