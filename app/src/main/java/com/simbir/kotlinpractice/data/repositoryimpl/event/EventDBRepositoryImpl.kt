63c45af8f793a39b4219d3afe17c92a8cc402aec
Vitaliy Nikolaev <faktor600gmail.com>
2020-09-30 18:09:58 +0400

    исправление repository и добавлена анотация Inject

--- app/src/main/java/com/simbir/kotlinpractice/data/repositoryimpl/event/EventDBRepositoryImpl.kt
+++ app/src/main/java/com/simbir/kotlinpractice/data/repositoryimpl/event/EventDBRepositoryImpl.kt
@@ -5,16 +5,19 @@ import com.simbir.kotlinpractice.data.db.map.DBEventMap
 import com.simbir.kotlinpractice.domain.Event
 import com.simbir.kotlinpractice.domain.repository.event.EventDBRepository
 import io.reactivex.rxjava3.core.Single
+import javax.inject.Inject
 
-class EventDBRepositoryImpl(
+class EventDBRepositoryImpl @Inject constructor(
+
     private val eventDao: EventDao,
     private val mapper: DBEventMap
+
 ): EventDBRepository {
 
     override fun getEventListFromDatabase(): Single<List<Event>> {
         return eventDao.getAll()
             .flattenAsFlowable { events -> events }
-            .map { event -> mapper.getEventFromDatabase(event) }
+            .map(mapper)
             .toList()
     }
 }
